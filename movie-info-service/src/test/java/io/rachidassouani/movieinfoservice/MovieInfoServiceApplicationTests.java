149c8aa6fe4c990d20cd954f986641b63c60bfb5
rachidassouani <rachidassouani@gmail.com>
2020-04-22 16:02:35 +0000

    Init 3 projects

---
+++ movie-info-service/src/test/java/io/rachidassouani/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
