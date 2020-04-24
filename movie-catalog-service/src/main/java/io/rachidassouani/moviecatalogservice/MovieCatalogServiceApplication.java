3400df5c1465781818d91d1f0d8d03741a51f827
rachidassouani <rachidassouani@gmail.com>
2020-04-24 03:06:03 +0000

    Use service discovery to call APIs

--- movie-catalog-service/src/main/java/io/rachidassouani/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/rachidassouani/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package io.rachidassouani.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +17,7 @@ public class MovieCatalogServiceApplication {
 
 
 	@Bean
+    @LoadBalanced
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
 	}
--- movie-catalog-service/src/main/java/io/rachidassouani/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/io/rachidassouani/moviecatalogservice/controller/MovieCatalogController.java
@@ -2,21 +2,16 @@ package io.rachidassouani.moviecatalogservice.controller;
 
 import io.rachidassouani.moviecatalogservice.model.CatalogItem;
 import io.rachidassouani.moviecatalogservice.model.Movie;
-import io.rachidassouani.moviecatalogservice.model.Rating;
 import io.rachidassouani.moviecatalogservice.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("catalog/")
@@ -28,7 +23,7 @@ public class  MovieCatalogController {
     @GetMapping("{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsData/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratingsData/users/" + userId, UserRating.class);
 
         return ratings.getUserRating()
                 .stream()
@@ -40,7 +35,7 @@ public class  MovieCatalogController {
                             .bodyToMono(Movie.class)
                             .block();
                      */
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/ " + rating.getMovieId(), Movie.class);
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/ " + rating.getMovieId(), Movie.class);
                     assert movie != null;
                     return new CatalogItem(movie.getName(), "Desc of " + movie.getMovieId(), rating.getRating());
                 })
